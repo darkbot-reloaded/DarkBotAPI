a7affa29852f29213b07fecc93dd207affc56dcf
Pablete1234 <pabloherrerapalacio@gmail.com>
2021-08-29 11:58:04 +0200

    Update configuration to get a config setting

--- api/src/main/java/eu/darkbot/api/extensions/Configurable.java
+++ api/src/main/java/eu/darkbot/api/extensions/Configurable.java
@@ -1,5 +1,7 @@
 package eu.darkbot.api.extensions;
 
+import eu.darkbot.api.config.ConfigSetting;
+
 /**
  * Marks this feature supporting user configuration.
  * The user configuration needs to be a custom java object that will be parsed for
@@ -8,5 +10,5 @@ package eu.darkbot.api.extensions;
  * @param <T> the type of configuration to use
  */
 public interface Configurable<T> {
-    void setConfig(T config);
+    void setConfig(ConfigSetting<T> config);
 }
