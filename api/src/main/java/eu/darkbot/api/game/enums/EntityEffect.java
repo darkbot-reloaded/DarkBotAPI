92cf967eae9bf9ea23695f4798f20957b17a9bbb
Marek <65947112+pajtor@users.noreply.github.com>
2021-11-06 06:53:09 +0100

    Adding Repair Bot into effects
    Also rearranging order
    + check this in order to add it too https://github.com/darkbot-reloaded/DarkBot/pull/87/commits/4b2776f73ec4454dd3850506a60d2fe48cb5bb83

--- api/src/main/java/eu/darkbot/api/game/enums/EntityEffect.java
+++ api/src/main/java/eu/darkbot/api/game/enums/EntityEffect.java
@@ -9,13 +9,12 @@ public enum EntityEffect {
     PET_SPAWN(2),
     ENERGY_LEECH(11),
     NPC_ISH(16),
-    BOX_COLLECTING(20),
-    BOOTY_COLLECTING(21),
     DRAW_FIRE(36),
-    ISH(84),
     STICKY_BOMB(56),
     POLARITY_POSITIVE(65),
     POLARITY_NEGATIVE(66),
+    REPAIR_BOT(76),
+    ISH(84),
     INFECTION(85);
 
     private final int id;
