ffcdf76695fe7520565ef1dcfd068a87a65a617a
Punikekk <macio521123@gmail.com>
2021-11-18 16:29:39 +0100

    Added owner in Pet

--- api/src/main/java/eu/darkbot/api/game/entities/Pet.java
+++ api/src/main/java/eu/darkbot/api/game/entities/Pet.java
@@ -1,5 +1,7 @@
 package eu.darkbot.api.game.entities;
 
+import java.util.Optional;
+
 /**
  * In-game pet entity flying on the map
  */
@@ -14,4 +16,9 @@ public interface Pet extends Ship {
      * @return userId of the {@link Pet}'s owner
      */
     int getOwnerId();
+
+    /**
+     * @return owner of this pet if present, {@link Optional#empty()} otherwise
+     */
+    Optional<Ship> getOwner();
 }
