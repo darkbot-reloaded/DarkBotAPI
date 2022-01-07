a346e484ab116e0845309bce44cf26ad4e664621
Punikekk <macio521123@gmail.com>
2022-01-07 11:51:49 +0100

    applied suggestion

--- api/src/main/java/eu/darkbot/api/extensions/IssueHandler.java
+++ api/src/main/java/eu/darkbot/api/extensions/IssueHandler.java
@@ -1,19 +1,33 @@
 package eu.darkbot.api.extensions;
 
+import org.jetbrains.annotations.NotNull;
+
 import java.util.Set;
 
 public interface IssueHandler {
 
-    Set<? extends Issue> getIssueSet();
+    @NotNull Set<? extends Issue> getIssueSet();
 
-    void addInfo(String message, String description);
-    void addWarning(String message, String description);
-    void addFailure(String message, String description);
+    void addIssue(@NotNull String message, @NotNull String description, @NotNull Level level);
+
+    default void addInfo(@NotNull String message, @NotNull String description) {
+        addIssue(message, description, Level.INFO);
+    }
+
+    default void addWarning(@NotNull String message, @NotNull String description) {
+        addIssue(message, description, Level.WARNING);
+
+    }
+
+    default void addFailure(@NotNull String message, @NotNull String description) {
+        addIssue(message, description, Level.ERROR);
+    }
 
     interface Issue {
-
         String getMessageKey();
+
         String getDescription();
+
         Level getIssueLevel();
     }
 
