ae258dbc04bec12ddf4393e185998bf59ec28518
Pablete1234 <pabloherrerapalacio@gmail.com>
2022-01-08 09:33:22 +0100

    Fix up npc flag implementing flags

--- api/src/main/java/eu/darkbot/api/config/types/NpcFlag.java
+++ api/src/main/java/eu/darkbot/api/config/types/NpcFlag.java
@@ -1,16 +1,16 @@
 package eu.darkbot.api.config.types;
 
 import eu.darkbot.api.config.annotations.Configuration;
-import eu.darkbot.api.extensions.NpcFlags;
 
 @Configuration("config.loot.npc_table.extra")
-public enum NpcFlag implements NpcFlags<NpcFlag> {
+public enum NpcFlag {
     NO_CIRCLE,
     IGNORE_OWNERSHIP,
     IGNORE_ATTACKED,
     IGNORE_BOXES,
     AGGRESSIVE_FOLLOW,
-    PASSIVE, ATTACK_SECOND,
+    PASSIVE,
+    ATTACK_SECOND,
     LEECH_ONLY,
     NO_SAB,
     USE_RSB,
