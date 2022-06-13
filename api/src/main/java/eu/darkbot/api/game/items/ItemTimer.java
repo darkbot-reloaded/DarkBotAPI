fe816a85ca48cf86e49a2a56e7ecf770c0d752f4
Punikekk <50504152+Punikekk@users.noreply.github.com>
2022-06-13 23:38:07 +0200

    Refactor Item api a bit

--- api/src/main/java/eu/darkbot/api/game/items/ItemTimer.java
+++ api/src/main/java/eu/darkbot/api/game/items/ItemTimer.java
@@ -1,20 +1,27 @@
 package eu.darkbot.api.game.items;
 
 public interface ItemTimer {
+    /**
+     * @return true if item is activated, false if is cooling down
+     */
+    boolean isActivated();
 
-    Type getTimerType();
-
-    default boolean isActivated() {
-        return getTimerType() == Type.ACTIVATED;
-    }
-
+    @Deprecated
     double getTotalCoolingTime();
 
-    double getTimeElapsed();
-    double getAvailableIn();
+    /**
+     * @return total time(ms) of activation/cooling down
+     */
+    double getTotalTime();
 
-    enum Type {
-        ACTIVATED,
-        COOLING_DOWN
-    }
+    /**
+     * @return time(ms) elapsed after activation/cooling down
+     */
+    double getTimeElapsed();
+
+    /**
+     * @return the amount of time(ms) item will be activated ({@link #isActivated()} == true,
+     * or time it takes to cooldown an item
+     */
+    double getAvailableIn();
 }
