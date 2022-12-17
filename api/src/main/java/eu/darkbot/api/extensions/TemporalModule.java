c0bd47218e0f61d53eb8c6f91b99e2bc4576d122
Pablo Herrera <pabloherrerapalacio@gmail.com>
2022-12-18 00:17:12 +0100

    Fix temporal module

--- api/src/main/java/eu/darkbot/api/extensions/TemporalModule.java
+++ api/src/main/java/eu/darkbot/api/extensions/TemporalModule.java
@@ -2,10 +2,12 @@ package eu.darkbot.api.extensions;
 
 /**
  * Represents a module that is only set for a short-lived task
- *
+ * <br>
  * They are not available for users to select, they're installed by other modules or behaviors,
- * and they will clean-up after themselves. If the user pauses and resumes the bot the temporal
+ * and they will clean up after themselves. If the user pauses and resumes the bot the temporal
  * modules also get reset to the proper module.
+ * <br>
+ * Extending {@code TemporalModule} in shared modules is recommended over implementing the interface yourself.
  */
 public interface TemporalModule {
 
@@ -13,5 +15,12 @@ public interface TemporalModule {
      * Go back to the previous module
      */
     void goBack();
+
+    /**
+     * The module to go back to, which must not be another temporal module,
+     * otherwise infinite recursion may occur.
+     * @return the previously used non-temporal module.
+     */
+    Module getBack();
 
 }
