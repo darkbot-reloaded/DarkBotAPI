06066b13c542b5f6e99144bc0d3b8e095dea898f
Pablo Herrera <pabloherrerapalacio@gmail.com>
2022-12-20 00:23:12 +0100

    Add http client methods to backpage API

--- api/src/main/java/eu/darkbot/api/managers/BackpageAPI.java
+++ api/src/main/java/eu/darkbot/api/managers/BackpageAPI.java
@@ -3,6 +3,8 @@ package eu.darkbot.api.managers;
 import eu.darkbot.api.API;
 import eu.darkbot.api.extensions.Task;
 import eu.darkbot.util.TimeUtils;
+import eu.darkbot.util.http.Http;
+import eu.darkbot.util.http.Method;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.UnknownNullability;
 
@@ -55,26 +57,106 @@ public interface BackpageAPI extends API.Singleton {
     Instant getLastRequestTime();
 
     /**
-     * Returns connection with current {@link #getInstanceURI()} &amp; path, with the sessionID cookie.
-     *
-     * @param path URL path &amp; query parameters to append to {@link #getInstanceURI()}
+     * Forces last request time to be updated to the current time
      */
-    HttpURLConnection getConnection(@NotNull String path) throws Exception;
+    void updateLastRequestTime();
 
     /**
      * Returns connection with current {@link #getInstanceURI()} &amp; path, with the sessionID cookie.
      *
+     * @param path URL path &amp; query parameters to append to {@link #getInstanceURI()}
+     *
+     * @deprecated Use {@link #getHttp(String)} instead
+     */
+    @Deprecated
+    HttpURLConnection getConnection(@NotNull String path) throws Exception;
+
+    /**
+     * Returns connection with current {@link #getInstanceURI()} &amp; path, with the sessionID cookie.
      *
      * @param path URL path &amp; query parameters to append to {@link #getInstanceURI()}
      * @param minWait Minimum time to wait since the last request in milliseconds.
      *                If the last request was over minWait ms ago, behavior
      *                is identical to {@link #getConnection(String)}, otherwise it will
      *                first sleep until enough time has passed.
+     *
+     * @deprecated Use {@link #getHttp(String, int)} instead
      */
+    @Deprecated
     default HttpURLConnection getConnection(@NotNull String path, int minWait) throws Exception {
         TimeUtils.sleepThread(getLastRequestTime().toEpochMilli() + minWait - System.currentTimeMillis());
 
         return getConnection(path);
+    }
+
+    /**
+     * Creates an HTTP client that connects to {@link #getInstanceURI()} &amp; path, with the sessionID cookie set.
+     * You may manipulate this further, adding parameters via the builder-style methods
+     *
+     * @param path The path to append at the end of the URI
+     * @param method HTTP method to call with
+     * @return An HTTP client to perform the connection with
+     */
+    default Http http(@NotNull String path, Method method) {
+        if (!isInstanceValid()) throw new UnsupportedOperationException("Can't connect when instance is invalid");
+
+        return Http.create(getInstanceURI() + path, method)
+                .setRawHeader("Cookie", "dosid=" + getSid())
+                .addSupplier(this::updateLastRequestTime);
+    }
+
+    /**
+     * Creates an HTTP client that connects to {@link #getInstanceURI()} &amp; path, with the sessionID cookie set.
+     * Will ensure at least {@param minWait} ms have elapsed since last request.
+     *
+     * @param path The path to append at the end of the URI
+     * @param method HTTP method to call with
+     * @param minWait minimum delay since last call, in milliseconds
+     * @return An HTTP client to perform the connection with
+     */
+    default Http http(@NotNull String path, Method method, int minWait) {
+        TimeUtils.sleepThread(getLastRequestTime().toEpochMilli() + minWait - System.currentTimeMillis());
+        return http(path, method);
+    }
+
+    /**
+     * Equivalent to {@link #http(String, Method)} with GET method.
+     *
+     * @param path The path to append at the end of the URI
+     * @return An HTTP client to perform the connection with
+     */
+    default Http getHttp(@NotNull String path) {
+        return http(path, Method.GET);
+    }
+
+    /**
+     * Equivalent to {@link #http(String, Method, int)} with GET method.
+     *
+     * @param path The path to append at the end of the URI
+     * @return An HTTP client to perform the connection with
+     */
+    default Http getHttp(@NotNull String path, int minWait) {
+        return http(path, Method.GET, minWait);
+    }
+
+    /**
+     * Equivalent to {@link #http(String, Method)} with POST method.
+     *
+     * @param path The path to append at the end of the URI
+     * @return An HTTP client to perform the connection with
+     */
+    default Http postHttp(@NotNull String path) {
+        return http(path, Method.POST);
+    }
+
+    /**
+     * Equivalent to {@link #http(String, Method, int)} with POST method.
+     *
+     * @param path The path to append at the end of the URI
+     * @return An HTTP client to perform the connection with
+     */
+    default Http postHttp(@NotNull String path, int minWait) {
+        return http(path, Method.POST, minWait);
     }
 
     /**
