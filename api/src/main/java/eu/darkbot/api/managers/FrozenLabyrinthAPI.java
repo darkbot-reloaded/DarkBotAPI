4d2794d988c8d9f83f2a07a2b851a8381c70ec21
Pablo Herrera <pabloherrerapalacio@gmail.com>
2022-12-23 23:46:48 +0100

    Add frozen labyrinth API

---
+++ api/src/main/java/eu/darkbot/api/managers/FrozenLabyrinthAPI.java
@@ -0,0 +1,148 @@
+package eu.darkbot.api.managers;
+
+import eu.darkbot.api.API;
+import org.jetbrains.annotations.Nullable;
+
+public interface FrozenLabyrinthAPI extends API.Singleton {
+
+    /**
+     * Get how long until the current {@link #getStatus()} changes.
+     * @return how long until the lab opens/closes, in seconds
+     */
+    double getRemainingTime();
+
+    /**
+     * In-game keys, they are required to jump portals inside the labyrinth
+     * @return Amount of keys available
+     */
+    int getKeyCount();
+
+    /**
+     * @return the current status of the frozen labyrinth
+     */
+    Status getStatus();
+
+    /**
+     * @return current map the synk is in, if any
+     */
+    @Nullable MapZone getSynkMapZone();
+
+    /**
+     * The current status of the labyrinth
+     */
+    enum Status {
+        OPEN, CLOSED, ENDED
+    }
+
+    /**
+     * List of all the maps inside the labyrinth.
+     * You can convert them to GameMap in {@link StarSystemAPI}
+     */
+    enum LabMap {
+        Atlas_A  (430),
+        Atlas_B  (431),
+        Atlas_C  (432),
+        Cygni    (433),
+        Helvetios(434),
+        Eridani  (435),
+        Sirius   (436),
+        Sadatoni (437),
+        Persei   (438),
+        Volantis (439),
+        Alcyone  (440),
+        Auriga   (441),
+        Bootes   (442),
+        Aquila   (443),
+        Orion    (444),
+        Maia     (445);
+
+        private final int mapId;
+        private final MapZone[] zones = new MapZone[4];
+
+        LabMap(int mapId) {
+            this.mapId = mapId;
+            for (int i = 0; i < 4; i++)
+                zones[i] = new MapZone(this, Zone.values()[i]);
+        }
+
+        /**
+         * Get the in-game map id, same as a corresponding {@link eu.darkbot.api.game.other.GameMap}
+         * @return the in-game map id
+         */
+        public int getMapId() {
+            return mapId;
+        }
+
+        /**
+         * Get a MapZone for this LabMap with one of the 4 corners
+         * @param corner The zone for the corner to get
+         * @return The map zone for this lab map + zone combination.
+         */
+        public MapZone z(Zone corner) {
+            return z(corner.ordinal());
+        }
+
+        /**
+         * Get a MapZone for this LabMap with one of the 4 corners
+         * @param corner The corner, must be one of 0, 1, 2 or 3
+         * @return The map zone for this lab map + zone combination.
+         */
+        public MapZone z(int corner) {
+            if (corner < 0 || corner >= zones.length)
+                throw new IllegalArgumentException("Corner must be between 0 and 3");
+            return zones[corner];
+        }
+    }
+
+    /**
+     * One of the 4 zones in the maps
+     */
+    enum Zone {
+        TOP_LEFT("TL"),
+        TOP_RIGHT("TR"),
+        BOTTOM_LEFT("BL"),
+        BOTTOM_RIGHT("BR");
+
+        private final String tinyName;
+
+        Zone(String tinyName) {
+            this.tinyName = tinyName;
+        }
+
+        public String getTinyName() {
+            return tinyName;
+        }
+    }
+
+    /**
+     * The quadrant or zone for a specific map
+     */
+    class MapZone {
+        private final LabMap map;
+        private final Zone zone;
+
+        /**
+         * Get instances via {@link LabMap#z(Zone)} or {@link LabMap#z(int)}
+         * @param map the map represented
+         * @param zone the zone inside the map
+         */
+        private MapZone(LabMap map, Zone zone) {
+            this.map = map;
+            this.zone = zone;
+        }
+
+        public LabMap getMap() {
+            return map;
+        }
+
+        public Zone getZone() {
+            return zone;
+        }
+
+        @Override
+        public String toString() {
+            return map.name().replace("_", " ") + " " + zone.ordinal();
+        }
+    }
+
+}
