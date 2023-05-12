1267dedddd0f81be71be65c5053cc81ae2b22a63
Anjesh Shrestha <anjeshshrestha@gmail.com>
2023-05-11 20:18:33 -0600

    add remaining method to expose

--- api/src/main/java/eu/darkbot/api/managers/NpcEventAPI.java
+++ api/src/main/java/eu/darkbot/api/managers/NpcEventAPI.java
@@ -14,12 +14,42 @@ public interface NpcEventAPI extends API.Singleton {
     double getRemainingTime();
 
     /**
+     * @return the current event name
+     */
+    String getEventName();
+
+    /**
+     * @return the current event id
+     */
+    String getEventID();
+
+    /**
+     * @return the message description during the event
+     */
+    String getEventDescription();
+
+    /**
+     * @return the message for npc's to kill
+     */
+    String getNpcLeftDescription();
+
+    /**
+     * @return the number of npc left on the map
+     */
+    int npcLeft();
+
+    /**
+     * @return the number of boss npc left on the map
+     */
+    int bossNpcLeft();
+
+    /**
      * @return the current status of the Npc event
      */
     Status getStatus();
 
     enum Status {
-        ACTIVE, INACTIVE, ENDED
+        ACTIVE, INACTIVE
     }
 
 }
