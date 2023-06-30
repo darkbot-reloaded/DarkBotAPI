cbf8fc12378d824f9e2da5c75f5496ae4c43f8d9
Anjesh Shrestha <anjeshshrestha@gmail.com>
2023-06-30 19:13:19 +0200

    consolidate dispatchapi

--- api/src/main/java/eu/darkbot/api/managers/DispatchAPI.java
+++ api/src/main/java/eu/darkbot/api/managers/DispatchAPI.java
@@ -15,6 +15,63 @@ public interface DispatchAPI extends API.Singleton {
     List<? extends RewardLoot> getRewardLoot();
 
     /**
+     * @return avaialbe dispatch slots  to be used
+     */
+    int getAvailableSlots();
+
+    /**
+     * @return total slots that can be used
+     */
+    int getTotalSlots();
+
+    /**
+     * @return The {@code List} of all available retrivers
+     */
+    List<? extends Retriever> getAvailableRetrievers();
+
+    /**
+     * @return The {@code List} of all in progress retrivers
+     */
+    List<? extends Retriever> getInProgressRetrievers();
+
+    /**
+     * @return The {@code DispatchRetrieverVO} of the selected retriever
+     */
+    Retriever getSelectedRetriever();
+
+    /**
+     * In game retriever representation, includes name, in game name, type, duration &amp; slot id
+     */
+    interface Retriever {
+        /**
+         * @return in game variable name
+         */
+
+        String getId();
+
+        /**
+         * @return short game name
+         */
+        String getName();
+
+        /**
+         * @return retriever type
+         */
+        String getDescription();
+
+        /**
+         * @return the time to build or complete retriever
+         */
+
+        double getDuration();
+
+        /**
+         * @return slot position of the retriever
+         */
+        int getSlotId();
+    }
+
+    /**
      * In game rewards loot representation, includes loot id, &amp; amount
      */
     interface RewardLoot {
