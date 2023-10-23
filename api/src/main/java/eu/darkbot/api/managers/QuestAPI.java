0e92219f8c9dd010444e25575611263d24ee62ce
dm94 <dm94@hotmail.es>
2023-10-23 23:06:41 +0200

    Updated QuestAPI

--- api/src/main/java/eu/darkbot/api/managers/QuestAPI.java
+++ api/src/main/java/eu/darkbot/api/managers/QuestAPI.java
@@ -1,6 +1,9 @@
 package eu.darkbot.api.managers;
 
 import eu.darkbot.api.API;
+
+import java.util.List;
+
 import org.jetbrains.annotations.Nullable;
 
 /**
@@ -13,7 +16,21 @@ public interface QuestAPI extends API.Singleton {
      *         Even if there are several accepted quests, only the selected one is
      *         returned.
      */
-    @Nullable Quest getCurrentQuest();
+    @Nullable
+    Quest getCurrentQuestDisplayed();
+
+    /**
+     * @return The quest that is currently selected in the QuestGiver
+     */
+    @Nullable
+    Quest getCurrentQuestSeleted();
+
+    /**
+     * @return Returns a list of current quests, only has data when QuestGiver is
+     *         opened
+     */
+    @Nullable
+    List<QuestListItem> getCurrestQuests();
 
     /**
      * Quest Object
@@ -49,5 +66,113 @@ public interface QuestAPI extends API.Singleton {
          * @return The number of conditions the quest has to be completed.
          */
         int getConditionsCount();
+
+        /**
+         * @return True if completed
+         */
+        boolean isCompleted();
+
+        /**
+         * @return List of conditions for completion
+         */
+        List<Contition> getConditions();
+
+        /**
+         * @return List of rewards to be obtained by completing the quest
+         */
+        List<Reward> getRewards();
+    }
+
+    /**
+     * QuestListItem Object
+     */
+    interface QuestListItem {
+
+        /**
+         * @return The Quest ID
+         */
+        int getId();
+
+        /**
+         * @return Level required to activate it
+         */
+        int getLevelRequired();
+
+        /**
+         * @return True if selected in the interface
+         */
+        boolean isSelected();
+
+        /**
+         * @return True if completed
+         */
+        boolean isCompleted();
+
+        /**
+         * @return Quest Title
+         */
+        String getTitle();
+
+        /**
+         * @return Type of quest
+         */
+        String getType();
+
+        /**
+         * @return True if it can be activated
+         */
+        boolean isActivable();
+    }
+
+    /**
+     * Contition Object
+     */
+    interface Contition {
+
+        /**
+         * @return Description of the condition
+         */
+        String getDescription();
+
+        /**
+         * @return Completed amount of the condition
+         */
+        double getGoalReached();
+
+        /**
+         * @return Amount needed to complete the condition
+         */
+        double getGoal();
+
+        /**
+         * @return True if the condition has been completed
+         */
+        boolean isCompleted();
+
+        /**
+         * @return Type of condition
+         */
+        String getConditionType();
+
+        /**
+         * @return Returns the list of conditions that the condition itself may have
+         */
+        List<Contition> getConditions();
+    }
+
+    /**
+     * Reward Object
+     */
+    interface Reward {
+
+        /**
+         * @return Amount to be obtained
+         */
+        int getAmount();
+
+        /**
+         * @return Type to be obtained
+         */
+        String getType();
     }
 }
