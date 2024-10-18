53dd0cd6f4036736ebfe53dc9faf27b440c2af0a
Punikekk <50504152+Punikekk@users.noreply.github.com>
2024-10-18 16:37:57 +0200

    Add API to select locator pick in PetGearSupplier (#102)

--- util/src/main/java/eu/darkbot/util/StringUtils.java
+++ util/src/main/java/eu/darkbot/util/StringUtils.java
@@ -2,10 +2,34 @@ package eu.darkbot.util;
 
 import lombok.experimental.UtilityClass;
 
+import java.util.Locale;
+import java.util.regex.Pattern;
+
 @UtilityClass
 public class StringUtils {
+    private static final Pattern NON_CHAR_REPLACEMENT = Pattern.compile("[^A-Za-z0-9]");
+    private static final Pattern MIMESIS_REPLACEMENT = Pattern.compile("m[i1]m[e3][s5][i1][s5]");
 
     public static String capitalize(String str) {
         return Character.toUpperCase(str.charAt(0)) + str.substring(1);
+    }
+
+    public static String fuzzyNpcName(String name) {
+        // xX[ Chaos Protegit ]Xx or -x-[ Synk ]-x-, make sure to remove the "x"
+        if ((name.startsWith("xX[") && name.endsWith("]Xx"))
+                || (name.startsWith("-x-") && name.endsWith("-x-"))) {
+            name = name.substring(3, name.length() - 3);
+        }
+        // Special case, keep upper & lower case
+        if (!name.contains("StreuneR")) {
+            name = name.toLowerCase(Locale.ROOT);
+        }
+        // Fix up referee bot
+        name = name.replace("referee binary bot", "referee bot");
+        // Fix up mimesis
+        name = MIMESIS_REPLACEMENT.matcher(name).replaceAll("mimesis");
+
+        // Keep only alphanumerical chars
+        return NON_CHAR_REPLACEMENT.matcher(name).replaceAll("");
     }
 }
